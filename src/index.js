3422657df482f04644ee81de20a23f5c4880fbfe
harrisrais <hr.rais.01@gmail.com>
2025-11-12 11:48:02 +0500

    Chnage Index file to use schema file

--- src/index.js
+++ src/index.js
@@ -1,20 +1,48 @@
-// src\index.js
+// // src\index.js
+// import express from 'express';
+// import { ApolloServer } from 'apollo-server-express';
+// import { connectDB } from './db.js';
+// import { expressMiddleware } from '@apollo/server/express4';
+// import { schema } from './graphql/schema.js';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const startServer = async () => {
+//   await connectDB();
+
+//   const app = express();
+//   app.use(cors());
+
+//   const server = new ApolloServer({
+//     schema,
+//   });
+
+//   await server.start();
+//   server.applyMiddleware({ app });
+
+//   app.listen({ port: 4000 }, () =>
+//    console.log('🚀 Server running at http://localhost:8000/graphql')
+//   );
+// };
+
+// startServer();
+
+/* old method */
 import express from 'express';
+
 import { ApolloServer } from 'apollo-server-express';
-import { typeDefs } from './typeDefs.js';
-import { resolvers } from './resolvers.js';
 import { connectDB } from './db.js';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
-
+import { schema } from './graphql/schema.js';
 
 const startServer = async () => {
   await connectDB();
 
   const app = express();
-
   const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+    schema,
     introspection: true, // allow the schema to be visible
     plugins: [ApolloServerPluginLandingPageLocalDefault({ embed: true })], // enable the Apollo Sandbox UI
   });
